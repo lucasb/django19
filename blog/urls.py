fb441f79fd8860d5f1100a2b8118c15b7d86c569
Lucas Boscaini <lucasboscaini@gmail.com>
2016-01-10 17:39:24 -0200

    Create form to manage posts.

--- blog/urls.py
+++ blog/urls.py
@@ -6,4 +6,6 @@ from . import views
 urlpatterns = [
     url(r'^$', views.post_list),
     url(r'^post/(?P<id>[0-9]+)/$', views.post_detail),
+    url(r'^post/new/$', views.post_new),
+    url(r'^post/(?P<id>[0-9]+)/edit$', views.post_edit),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
 
 from .models import Post
+from .forms import PostForm
+
 
 def post_list(request):
     posts = Post.objects.filter(published_date__lte = timezone.now())\
@@ -11,3 +13,28 @@ def post_list(request):
 def post_detail(request, id):
     post = get_object_or_404(Post, id=id)
     return render(request, 'blog/post_detail.html', {'post': post})
+
+def post_new(request):
+    if request.method == 'POST':
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.save()
+            return redirect('blog.views.post_detail', id=post.id)
+    else:
+        form = PostForm()
+    return render(request, 'blog/post_edit.html', { 'form': form })
+
+def post_edit(request, id):
+    post = get_object_or_404(Post, id=id)
+    if request.method == 'POST':
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.save()
+            redirect('blog.views.post_detail', id=post.id)
+    else:
+        form = PostForm(instance=post)
+    return render(request, 'blog/post_edit.html', { 'form': form })
