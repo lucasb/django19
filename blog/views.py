1451be0be3b6fc5b7801088087ae99203e26b550
Lucas Boscaini <lucasboscaini@gmail.com>
2016-01-10 23:31:34 -0200

    Add posts list anc change color in style.

--- blog/views.py
+++ blog/views.py
@@ -11,10 +11,14 @@ def post_list(request):
     return render(request, 'blog/post_list.html', {'posts': posts})
 
 def post_detail(request, id):
+    posts = Post.objects.filter(published_date__lte = timezone.now())\
+                        .order_by('published_date')
     post = get_object_or_404(Post, id=id)
-    return render(request, 'blog/post_detail.html', {'post': post})
+    return render(request, 'blog/post_detail.html', {'post': post, 'posts': posts})
 
 def post_new(request):
+    posts = Post.objects.filter(published_date__lte = timezone.now())\
+                        .order_by('published_date')
     if request.method == 'POST':
         form = PostForm(request.POST)
         if form.is_valid():
@@ -24,10 +28,12 @@ def post_new(request):
             return redirect('blog.views.post_detail', id=post.id)
     else:
         form = PostForm()
-    return render(request, 'blog/post_edit.html', { 'form': form })
+    return render(request, 'blog/post_edit.html', {'form': form, 'posts': posts})
 
 def post_edit(request, id):
     post = get_object_or_404(Post, id=id)
+    posts = Post.objects.filter(published_date__lte = timezone.now())\
+                        .order_by('published_date')
     if request.method == 'POST':
         form = PostForm(request.POST, instance=post)
         if form.is_valid():
@@ -37,4 +43,4 @@ def post_edit(request, id):
             return redirect('blog.views.post_detail', id=post.id)
     else:
         form = PostForm(instance=post)
-    return render(request, 'blog/post_edit.html', { 'form': form })
+    return render(request, 'blog/post_edit.html', {'form': form, 'posts': posts})
