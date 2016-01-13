27241d0264fe99681c7d1fc3fdf58e60ed72bf5a
Lucas Boscaini <lucasboscaini@gmail.com>
2016-01-13 00:49:05 -0200

    Add form to comments posts.

--- blog/urls.py
+++ blog/urls.py
@@ -11,4 +11,7 @@ urlpatterns = [
     url(r'^post/(?P<id>[0-9]+)/delete/$', views.post_delete),
     url(r'^post/new/$', views.post_new),
     url(r'^post/drafts/$', views.post_draft_list),
+    url(r'^post/(?P<id>[0-9]+)/comment/$', views.add_comment_to_post, name='add_comment_to_post'),
+    url(r'^comment/(?P<id>[0-9]+)/approve/$', views.comment_approve, name='comment_approve'),
+    url(r'^comment/(?P<id>[0-9]+)/remove/$', views.comment_remove, name='comment_remove'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -2,8 +2,8 @@ from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
 from django.contrib.auth.decorators import login_required
 
-from .models import Post
-from .forms import PostForm
+from .models import Post, Comment
+from .forms import PostForm, CommentForm
 
 
 def post_list(request):
@@ -15,7 +15,18 @@ def post_detail(request, id):
     posts = Post.objects.filter(published_date__lte = timezone.now())\
                         .order_by('published_date')
     post = get_object_or_404(Post, id=id)
-    return render(request, 'blog/post_detail.html', {'post': post, 'posts': posts})
+    comment_form = CommentForm()
+    return render(request, 'blog/post_detail.html', {'post': post, 'posts': posts, 'form': comment_form})
+
+def add_comment_to_post(request, id):
+    post = get_object_or_404(Post, id=id)
+    if request.method == 'POST':
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.post = post
+            comment.save()
+            return redirect('blog.views.post_detail', id=id)
 
 @login_required
 def post_new(request):
@@ -66,3 +77,16 @@ def post_delete(request, id=id):
     post = get_object_or_404(Post, id=id)
     post.delete()
     return redirect('blog.views.post_list')
+
+@login_required
+def comment_approve(request, id=id):
+    comment = get_object_or_404(Comment, id=id)
+    comment.approve()
+    return redirect('blog.views.post_detail', id=comment.post.id)
+
+@login_required
+def comment_remove(request, id=id):
+    comment = get_object_or_404(Comment, id=id)
+    post_id = comment.post.id
+    comment.delete()
+    return redirect('blog.views.post_detail', id=post_id)
