3274b3177138fa11b53e6c62d5f4c9c077a3ef85
Lucas Boscaini <lucasboscaini@gmail.com>
2016-02-01 00:45:59 -0200

    logging support and start tdd.

--- blog/tests.py
+++ blog/tests.py
@@ -1,3 +1,14 @@
-from django.test import TestCase
+from django.test import TestCase, RequestFactory
+
+from .views import post_detail
 
 # Create your tests here.
+class BlogTest(TestCase):
+
+    def setUp(self):
+        self.factory = RequestFactory()
+
+    def test_post_detail(self):
+        request = self.factory.get('/post/1')
+        response = post_detail(request)
+        self.assertEqual(response.status_code, 200)
--- mysite/views.py
+++ mysite/views.py
@@ -1,3 +1,5 @@
+import logging
+
 from django.shortcuts import render
 from django.template import RequestContext
 
@@ -6,6 +8,7 @@ def not_authorazed(request):
     return render(request, 'error/status.html', {'code': 400, 'message': 'Not Authorized'}, status=400)
 
 def page_not_found(request):
+    logging.error('Item was not found.')
     return render(request, 'error/status.html', {'code': 404, 'message': 'Not Found'}, status=404)
 
 def server_error(request):
